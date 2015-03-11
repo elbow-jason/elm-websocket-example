2ed55d57dcc63b03b3ff8a5f3fa68d47c9d1a8fd
Jason Goldberger <jlgoldb2@asu.edu>
2015-03-10 23:03:21 -0700

    added simple ws server for trying out the elm stuff

---
+++ simple-websocket-server.js
@@ -0,0 +1,20 @@
+var ws = require("nodejs-websocket")
+
+// Scream server example: "hi" -> "HI!!!"
+var server = ws.createServer(function (conn) {
+    console.log("New connection")
+    var time_ping  = setInterval(function(){
+      var now = new Date().toJSON();
+      conn.sendText(now);
+      console.log("sent: " + now);
+    }, 10000);
+
+    conn.on("text", function (str) {
+        console.log("Received "+str)
+        conn.sendText(str.toUpperCase()+"!!!")
+    })
+    conn.on("close", function (code, reason) {
+        clearInterval(time_ping); // stop the server from crashin
+        console.log("Connection closed")
+    })
+}).listen(8001)
